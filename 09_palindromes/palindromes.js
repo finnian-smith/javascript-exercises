9402ed6afaf388753b5eb4c53035ef2e3df9034e
finnian-smith <finniansmith01@gmail.com>
2024-02-01 15:23:21 +0000

    Complete exercises 08 - 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let processed = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversed = processed.split("").reverse().join("");
 
+  return processed == reversed;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (typeof num === "string") {
+    num = parseInt(num);
+  }
 
+  if (num == 0) {
+    return 0;
+  } else if (num < 0) {
+    return "OOPS";
+  }
+  // Logic ->
+  // first = n - 2 , second = n - 1
+
+  // current = 0 + 1
+  // first = second
+  // second = current
+
+  let first = 0;
+  let second = 1;
+
+  for (let i = 2; i <= num; i++) {
+    let current = first + second;
+    first = second;
+    second = current;
+  }
+
+  return second;
 };
 
 // Do not edit below this line
